e502f536a23576400562ad1c97f468221149f002
oscar <oscar@ubuntu>
2014-04-23 11:24:34 -0700

    Merge origin/master
    Conflicts:
    	main.c
    	token.y
    	y.tab.c

--- nbproject/Makefile-Debug.mk
+++ nbproject/Makefile-Debug.mk
@@ -70,11 +70,6 @@ ${OBJECTDIR}/Tabla.o: Tabla.c
 	${RM} "$@.d"
 	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Tabla.o Tabla.c
 
-${OBJECTDIR}/Tabla.o: Tabla.c 
-	${MKDIR} -p ${OBJECTDIR}
-	${RM} "$@.d"
-	$(COMPILE.c) -g -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Tabla.o Tabla.c
-
 ${OBJECTDIR}/lex.yy.o: lex.yy.c 
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
